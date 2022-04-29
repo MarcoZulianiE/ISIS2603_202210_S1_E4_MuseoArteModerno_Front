b999bb29cbe7f944130380514f08e71a2ef45899
Marco <m.zuliani@uniandes.edu.co>
2022-04-29 08:41:37 -0500

    Quitar comentarios

--- src/app/artista/artista-detail.ts
+++ src/app/artista/artista-detail.ts
@@ -1,20 +1,20 @@
 import {Artista} from "./artista";
-//import { Pais } from "../pais/pais";
+import { Pais } from "../pais/pais";
 import { Movimientoartistico } from "../movimientoartistico/movimientoartistico";
-//import {Obra} from "../obra/obra";
-//Import {Museo} from "../museo/museo";
+import {Obra} from "../obra/obra";
+//import {Museo} from "../museo/museo";
 
 export class ArtistaDetail extends Artista{
   movimientos: Array<Movimientoartistico> = [];
-  //obras: Array<Obra> = [];
+  obras: Array<Obra> = [];
   //museos: Array<Museo> = [];
 
-  constructor(id: number, nombre: string ,imagen: string ,fechaNacimiento: Date, fechaFallecimiento: Date/*, lugarNacimiento: Pais, lugarFallecimiento: Pais*/
-              , movimientos: Array<Movimientoartistico>/*, obras: Array<Obra>, museos: Array<Museo>*/)
+  constructor(id: number, nombre: string ,imagen: string ,fechaNacimiento: Date, fechaFallecimiento: Date, lugarNacimiento: Pais, lugarFallecimiento: Pais
+              , movimientos: Array<Movimientoartistico>, obras: Array<Obra>/*, museos: Array<Museo>*/)
   {
-    super(id, nombre, imagen, fechaNacimiento, fechaFallecimiento/*, lugarNacimiento, lugarFallecimiento*/);
+    super(id, nombre, imagen, fechaNacimiento, fechaFallecimiento, lugarNacimiento, lugarFallecimiento);
     this.movimientos = movimientos;
-    //this.obras = obras;
+    this.obras = obras;
     //this.museos = museos;
   }
 }
--- src/app/artista/artista.ts
+++ src/app/artista/artista.ts
@@ -1,4 +1,4 @@
-//import {Pais} from "../pais/pais";
+import {Pais} from "../pais/pais";
 
 export class Artista {
   id: number;
@@ -6,18 +6,18 @@ export class Artista {
   imagen: string;
 	fechaNacimiento: Date;
 	fechaFallecimiento: Date;
-	/*lugarNacimiento: Pais;
-	lugarFallecimiento: Pais;*/
+	lugarNacimiento: Pais;
+	lugarFallecimiento: Pais;
 
-  constructor(id: number, nombre: string , imagen: string, fechaNacimiento: Date, fechaFallecimiento: Date/*, lugarNacimiento: Pais, lugarFallecimiento: Pais*/)
+  constructor(id: number, nombre: string , imagen: string, fechaNacimiento: Date, fechaFallecimiento: Date, lugarNacimiento: Pais, lugarFallecimiento: Pais)
   {
     this.id = id;
     this.nombre = nombre;
     this.imagen = imagen;
     this.fechaNacimiento = fechaNacimiento;
     this.fechaFallecimiento = fechaFallecimiento;
-    /*this.lugarNacimiento = lugarNacimiento;
-    this.lugarFallecimiento = lugarFallecimiento;*/
+    this.lugarNacimiento = lugarNacimiento;
+    this.lugarFallecimiento = lugarFallecimiento;
   }
 }
 
