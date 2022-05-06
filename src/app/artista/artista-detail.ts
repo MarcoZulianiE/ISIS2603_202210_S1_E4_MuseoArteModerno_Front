437f57efcc28a3c105f4e30ee95a89dd01a3c5e6
Marco <m.zuliani@uniandes.edu.co>
2022-05-05 20:06:17 -0500

    Quitar comentarios asociados con Museo

--- src/app/artista/artista-detail.ts
+++ src/app/artista/artista-detail.ts
@@ -2,19 +2,19 @@ import {Artista} from "./artista";
 import { Pais } from "../pais/pais";
 import { Movimientoartistico } from "../movimientoartistico/movimientoartistico";
 import {Obra} from "../obra/obra";
-//import {Museo} from "../museo/museo";
+import {Museo} from "../museo/museo";
 
 export class ArtistaDetail extends Artista{
   movimientos: Array<Movimientoartistico> = [];
   obras: Array<Obra> = [];
-  //museos: Array<Museo> = [];
+  museos: Array<Museo> = [];
 
   constructor(id: number, nombre: string ,imagen: string ,fechaNacimiento: Date, fechaFallecimiento: Date, lugarNacimiento: Pais, lugarFallecimiento: Pais
-              , movimientos: Array<Movimientoartistico>, obras: Array<Obra>/*, museos: Array<Museo>*/)
+              , movimientos: Array<Movimientoartistico>, obras: Array<Obra>, museos: Array<Museo>)
   {
     super(id, nombre, imagen, fechaNacimiento, fechaFallecimiento, lugarNacimiento, lugarFallecimiento);
     this.movimientos = movimientos;
     this.obras = obras;
-    //this.museos = museos;
+    this.museos = museos;
   }
 }
