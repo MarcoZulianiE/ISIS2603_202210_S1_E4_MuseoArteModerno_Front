5d04a9184206891e4a07766ab67824bda4efa8de
MussoV <s.rodriguez52@uniandes.edu.co>
2022-05-07 16:14:18 -0500

    Modificacion Ciudad para Coordenadas
    debido al mapbox y la idea que tenemos de los marcadores de ciudad hay que cambiar coordenadas en formato lan,lot tanto en front como en back

--- src/app/ciudad/ciudad.ts
+++ src/app/ciudad/ciudad.ts
@@ -1,10 +1,14 @@
 export class Ciudad {
   nombre:string;
   id:number;
+  lat:number;
+  lon:number;
 
-	constructor(nombre:string , id:number) {
+	constructor(nombre:string , id:number,lat:number,lon:number) {
     this.nombre=nombre;
     this.id=id;
+    this.lat=lat;
+    this.lon=lon;
 	}
 
 }
