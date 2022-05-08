53ccaeb6585e43c8847139577f52f13eff752474
MussoV <s.rodriguez52@uniandes.edu.co>
2022-05-08 03:00:05 -0500

    implementacion prueba detalle

--- src/app/ciudad/ciudad-detail/ciudad-detail.component.spec.ts
+++ src/app/ciudad/ciudad-detail/ciudad-detail.component.spec.ts
@@ -4,10 +4,16 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { CiudadDetailComponent } from './ciudad-detail.component';
+import { faker } from '@faker-js/faker';
+import { CiudadDetail} from "../ciudad-detail";
+import { Pais } from 'src/app/pais/pais';
+
 
 describe('CiudadDetailComponent', () => {
   let component: CiudadDetailComponent;
   let fixture: ComponentFixture<CiudadDetailComponent>;
+  let debug: DebugElement;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,10 +25,23 @@ describe('CiudadDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CiudadDetailComponent);
     component = fixture.componentInstance;
+    let pais =new Pais(  faker.datatype.number(),  faker.lorem.sentence(),  faker.lorem.sentence() )
+    component.ciudadDetail = new CiudadDetail( faker.datatype.number(),  faker.lorem.sentence(),   faker.image.imageUrl(), faker.lorem.sentence(), pais);
     fixture.detectChanges();
+    debug = fixture.debugElement;
+
+
+  });
+
+
+  it('should have an img element', () => {
+    expect(debug.query(By.css('img')).attributes['alt']).toEqual(
+      component.ciudadDetail.nombre
+    );
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/ciudad/ciudad.service.spec.ts
+++ src/app/ciudad/ciudad.service.spec.ts
@@ -3,6 +3,7 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { CiudadService } from './ciudad.service';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { faker } from '@faker-js/faker';
 
 describe('Service: Ciudad', () => {
   beforeEach(() => {
