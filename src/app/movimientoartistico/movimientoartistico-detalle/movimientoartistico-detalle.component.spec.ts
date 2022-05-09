a7058f8e6f22ab4f0c45909d6859838737c0b4d7
juanspardor <78055237+juanspardor@users.noreply.github.com>
2022-05-08 23:33:01 -0500

    Pruebas de componentes de movimiento artistico

--- src/app/movimientoartistico/movimientoartistico-detalle/movimientoartistico-detalle.component.spec.ts
+++ src/app/movimientoartistico/movimientoartistico-detalle/movimientoartistico-detalle.component.spec.ts
@@ -2,12 +2,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { MovimientoartisticoDetalleComponent } from './movimientoartistico-detalle.component';
+import {MovimientoartisticoDetail} from "../movimientoartistico-detail";
+import { Pais } from 'src/app/pais/pais';
 
 describe('MovimientoartisticoDetalleComponent', () => {
   let component: MovimientoartisticoDetalleComponent;
   let fixture: ComponentFixture<MovimientoartisticoDetalleComponent>;
+  let debug = DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,9 +24,22 @@ describe('MovimientoartisticoDetalleComponent', () => {
     fixture = TestBed.createComponent(MovimientoartisticoDetalleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    let pais = new Pais(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.image.imageUrl()
+     );
+
+    component.movimientoDetail = new MovimientoartisticoDetail(faker.datatype.number(), faker.lorem.sentence(), faker.image.imageUrl(), faker.lorem.sentence(), faker.date.pas(), [], [], [], pais);
+    fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
--- src/app/movimientoartistico/movimientoartistico-list/movimientoartistico-list.component.spec.ts
+++ src/app/movimientoartistico/movimientoartistico-list/movimientoartistico-list.component.spec.ts
@@ -2,16 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { MovimientoartisticoListComponent } from './movimientoartistico-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MovimientoartisticoDetail} from "../movimientoartistico-detail";
+import { Pais } from 'src/app/pais/pais';
+import {MovimientoartisticoService} from '../movimientoartistico.service'
 
-describe('MovimientoartisticoListComponent', () => {
+describe('MovimientoartisticoDetalleComponent', () => {
   let component: MovimientoartisticoListComponent;
   let fixture: ComponentFixture<MovimientoartisticoListComponent>;
+  let debug = DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MovimientoartisticoListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ MovimientoartisticoListComponent ],
+      providers: [MovimientoartisticoService]
     })
     .compileComponents();
   }));
@@ -20,9 +28,23 @@ describe('MovimientoartisticoListComponent', () => {
     fixture = TestBed.createComponent(MovimientoartisticoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    let pais = new Pais(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.image.imageUrl()
+     );
+
+  component.artistas = [new MovimientoartisticoDetail(faker.datatype.number(), faker.lorem.sentence(), faker.image.imageUrl(), faker.lorem.sentence(), faker.date.pas(), [], [], [], pais)];
+  fixture.detectChanges();
+  debug = fixture.debugElement;
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
