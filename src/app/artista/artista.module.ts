b3a586c4373e7a3168151c4714465f471a555beb
Marco <m.zuliani@uniandes.edu.co>
2022-05-10 21:23:14 -0500

    Updates Routing

--- src/app/artista/artista.module.ts
+++ src/app/artista/artista.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtistaListComponent } from './artista-list/artista-list.component';
 import { ArtistaDetailComponent } from './artista-detail/artista-detail.component';
+import { ArtistaRoutingModule } from './artista-routing.module';;
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ArtistaRoutingModule
   ],
   declarations: [ArtistaListComponent,ArtistaDetailComponent],
   exports: [ArtistaListComponent,ArtistaDetailComponent]
--- src/app/movimientoartistico/movimientoartistico.module.ts
+++ src/app/movimientoartistico/movimientoartistico.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MovimientoartisticoListComponent} from './movimientoartistico-list/movimientoartistico-list.component';
 import {MovimientoartisticoDetalleComponent} from './movimientoartistico-detalle/movimientoartistico-detalle.component';
+import { MovimientoArtisticoRoutingModule } from './movimientoartistico-routing.module';;
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MovimientoArtisticoRoutingModule
   ],
   declarations: [MovimientoartisticoListComponent, MovimientoartisticoDetalleComponent],
   exports: [MovimientoartisticoListComponent]
