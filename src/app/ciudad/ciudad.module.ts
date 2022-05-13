e4b56f5c5a192d114319d279a44bc5a5bcca9932
Marco <m.zuliani@uniandes.edu.co>
2022-05-13 08:57:28 -0500

    Ciudad Routing

--- src/app/ciudad/ciudad.module.ts
+++ src/app/ciudad/ciudad.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CiudadListComponent } from './ciudad-list/ciudad-list.component';
 import { CiudadDetailComponent } from './ciudad-detail/ciudad-detail.component';
+import { CiudadRoutingModule } from './ciudad-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CiudadRoutingModule
   ],
   declarations: [CiudadListComponent,CiudadDetailComponent],
   exports:[CiudadListComponent]
