0e6378647c90200ac965d00d50d887a8e3aed424
juanspardor <78055237+juanspardor@users.noreply.github.com>
2022-06-04 18:56:59 -0500

    Cambios faltantes completos para que movimiento artistico este funcional

--- src/app/movimientoartistico/movimientoartistico-list/movimientoartistico-list.component.ts
+++ src/app/movimientoartistico/movimientoartistico-list/movimientoartistico-list.component.ts
@@ -9,22 +9,59 @@ import { MovimientoartisticoService } from '../movimientoartistico.service';
   styleUrls: ['./movimientoartistico-list.component.css']
 })
 export class MovimientoartisticoListComponent implements OnInit {
+
+  tipoSort: string = 'A..Z';
   movimientos: Array<MovimientoartisticoDetail> = [];
   selected: boolean = false;
   selectedMovimiento!: MovimientoartisticoDetail;
 
   constructor(private movimientoService: MovimientoartisticoService) { }
 
-  onSelected(pMovimiento: MovimientoartisticoDetail):void{
-    this.selected = true;
-    this.selectedMovimiento = pMovimiento;
-
+  onSelected(tipo: string):void{
+    this.tipoSort = tipo;
+    this.ordenar();
   }
+
+  ordenar () : void {
+    if (this.tipoSort=='A..Z') {
+      this.movimientos = this.movimientos.sort((a,b)=> {
+        if(a.nombre > b.nombre) return 1;
+        if(a.nombre <= b.nombre) return -1;
+        return 0;
+      });
+    } else if (this.tipoSort=='Z..A') {
+      this.movimientos = this.movimientos.sort((a,b)=> {
+        if(a.nombre < b.nombre) return 1;
+        if(a.nombre >= b.nombre) return -1;
+        return 0;
+      });
+    }
+  }
+
   getMovimientos(){
     this.movimientoService.getMovimientosArtisticos().subscribe((recibidos) =>{
       this.movimientos = recibidos;
+      this.ordenar();
     });
   }
+
+  getMovimientosFiltrados(substr: any): void{
+    this.movimientoService.getMovimientosArtisticos().subscribe( (recibidos) => {
+      this.movimientos = recibidos.filter( (a) => {
+        return a.nombre.toLowerCase().match(substr.toLowerCase())
+      })
+    });
+  }
+
+  filtrar() {
+    if (document.querySelector('input')?.value == '') {
+      this.getMovimientos();
+    } else {
+      this.getMovimientosFiltrados(document.querySelector('input')?.value);
+    }
+  }
+
+
   ngOnInit() {
     this.getMovimientos();
   }
